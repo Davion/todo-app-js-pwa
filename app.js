0cf064db47f2585bde32ee79b578890ccf786a5c
Klajdi Hoxha <klainti95@gmail.com>
2021-01-09 12:35:52 +0100

    remove todo and check todo complete

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //Functions
 function addTodo(event){
@@ -32,4 +33,24 @@ function addTodo(event){
 	todoList.appendChild(todoDiv);
 	//Clear Todo INPUT VALUE;
 	todoInput.value = "";
+}
+
+function deleteCheck(event){
+	const item = event.target;
+
+	//DELETE TODO
+	if(item.classList[0] === "trash-btn"){
+		const todo = item.parentElement;
+		//Animation
+		todo.classList.add("fall");
+		todo.addEventListener("transitionend", function(){
+			todo.remove();
+		});
+	}
+
+	//CHECK MARK
+	if(item.classList[0] === "complete-btn"){
+		const todo = item.parentElement;
+		todo.classList.toggle("completed");
+	}
 }
