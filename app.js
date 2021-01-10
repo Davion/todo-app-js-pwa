366983ddb12a7edda56895706494d37879df52f6
Klajdi Hoxha <klainti95@gmail.com>
2021-01-10 14:04:42 +0100

    filter options (select item) functionality complete

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 //Functions
 function addTodo(event){
@@ -52,5 +54,30 @@ function deleteCheck(event){
 	if(item.classList[0] === "complete-btn"){
 		const todo = item.parentElement;
 		todo.classList.toggle("completed");
-	}
+	}	
+}
+
+function filterTodo(event){
+	const todos = todoList.childNodes;
+	todos.forEach(function(todo){
+		switch(event.target.value){
+			case "all":
+				todo.style.display = "flex";
+				break;
+			case "completed":
+				if(todo.classList.contains("completed")){
+					todo.style.display = "flex";
+				}else{
+					todo.style.display = "none";
+				}
+				break;
+			case "uncompleted":
+				if(!todo.classList.contains("completed")){
+					todo.style.display = "flex";
+				}else{
+					todo.style.display = "none";
+				}
+				break;
+		}
+	});
 }
